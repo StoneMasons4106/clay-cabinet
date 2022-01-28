ecf85e40bd9f71032989bd030338adcd9b5a4e07
StoneMasons4106 <jake.d.rubach@gmail.com>
2022-01-28 17:39:00 -0500

    Another attempted fix for product carousels.

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,10 +1,16 @@
 $('.product-item').on("click", function() {
-    let productCarouselIndicatorsValue = $(this).children('#product').attr('data-bs-target');
-    let productCarouselIndicators = $(productCarouselIndicatorsValue).children('#product-carousel-indicators');
-    let productCarouselItems = $(productCarouselIndicatorsValue).children('.carousel-item');
-    
-    for (index in productCarouselItems) {
-        productCarouselIndicators.html("<li data-bs-target='#productCarousel' data-bs-slide-to='" + index + "' class='active'></li>");
-        productCarouselIndicators.html("<li data-bs-target='#productCarousel' data-bs-slide-to='" + index + "'></li>");
-    };
+
+    /**
+   * Product Carousel Indicators
+   */
+   let productCarouselIndicators = $(this).children("#product-carousel-indicators");
+   let productCarouselItems = $(this).children('.carousel-item');
+
+   for (i in productCarouselItems) {
+    if (i === 0) {
+      productCarouselIndicators.html("<li data-bs-target='#productCarousel' data-bs-slide-to='" + i + "' class='active'></li>");
+    } else {
+      productCarouselIndicators.html("<li data-bs-target='#productCarousel' data-bs-slide-to='" + i + "'></li>");
+    }
+  };
 });
