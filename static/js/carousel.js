ae5321d29800b789efde47edf174b3cb29d535fb
StoneMasons4106 <jake.d.rubach@gmail.com>
2022-01-28 22:24:49 -0500

    Using find instead of children for jquery carousel transitions.

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,14 +1,11 @@
 $('.product-item').on("click", function() {
-
-    /**
-   * Product Carousel Indicators
-   */
-   let productCarouselIndicatorsValue = $(this).children(".fancybox").attr('data-bs-target');
-   let productCarouselIndicators = $(productCarouselIndicatorsValue).children('.carousel-indicators')
-   let productCarouselItems = $(productCarouselIndicatorsValue).children('.carousel-item');
+  
+   var productCarouselIndicatorsValue = $(this).children(".fancybox").attr('data-bs-target').replace('#','');
+   var productCarouselIndicators = $("#" + productCarouselIndicatorsValue).find('#product-carousel-indicators');
+   var productCarouselItems = $("#" + productCarouselIndicatorsValue).find('.carousel-item');
 
    for (i in productCarouselItems) {
-    if (i === 0) {
+    if (i == 0) {
       productCarouselIndicators.html("<li data-bs-target='#productCarousel' data-bs-slide-to='" + i + "' class='active'></li>");
     } else {
       productCarouselIndicators.html("<li data-bs-target='#productCarousel' data-bs-slide-to='" + i + "'></li>");
