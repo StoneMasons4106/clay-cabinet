b0083274e87ef22d5e08d3080bae8df44813f7c9
StoneMasons4106 <jake.d.rubach@gmail.com>
2022-01-30 12:14:19 -0500

    Style changes to the carousel, functioning search bar in the products page. Styling to come later.

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import Content, Product, Category
+from django.db.models import Q
 
 # Create your views here.
 
@@ -8,6 +9,7 @@ def all_products(request):
     content = Content.objects.values()[0]
     products = Product.objects.all()
     categories = Category.objects.values()
+    query = None
 
     if request.GET:
         if 'category' in request.GET:
@@ -15,11 +17,20 @@ def all_products(request):
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
+        if 'query' in request.GET:
+            query = request.GET['query']
+            if not query:
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
     context = {
         'page': 'products',
         'content': content,
         'products': products,
         'current_categories': categories,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
