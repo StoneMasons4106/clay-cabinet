9b63887cfbcd0c0dbe9e580b3d9a46fb59c880e0
StoneMasons4106 <jake.d.rubach@gmail.com>
2022-03-10 19:44:04 -0500

    Basic profile page displaying.

---
+++ profiles/forms.py
@@ -0,0 +1,63 @@
+from django import forms
+from .models import UserProfile
+from django.contrib.auth.models import User
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_county': 'County, State or Locality',
+        }
+
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].label = False
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        exclude = ('password', 'last_login', 'is_superuser', 'is_staff', 
+        'is_active', 'date_joined', 'groups', 'user_permissions',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'username': 'Username',
+            'email': 'Email',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+        }
+
+        self.fields['username'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].label = False
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+from .forms import UserProfileForm, UserForm
+from django.contrib.auth.models import User
+from allauth.account.models import EmailAddress
 
-# Create your views here.
+
+@login_required
+def profile(request):
+    """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    user = get_object_or_404(User, username=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        form_two = UserForm(request.POST, instance=user)
+        if form.is_valid() and form_two.is_valid():
+            try:
+                user_email = get_object_or_404(EmailAddress, user_id=request.user)
+                if str(request.POST.get('email')) != str(user_email):
+                    new_email = request.POST.get('email')
+                    profile.add_email_address(request, new_email)
+                else:
+                    pass
+                form.save()
+                form_two.save()
+            except EmailAddress.MultipleObjectsReturned:
+                pass
+        else:
+            pass
+    else:
+        form = UserProfileForm(instance=profile)
+        form_two = UserForm(instance=user)
+    
+    orders = profile.orders.all() 
+    
+    template = 'profiles/profile.html'
+    context = {
+        'page': 'profile',
+        'profile': profile,
+        'user': user,
+        'orders': orders,
+        'on_profile_page': True,
+    }
+
+    return render(request, template, context)
