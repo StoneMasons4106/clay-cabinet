453ecc18527bab89806e66c47f6b40415961384d
StoneMasons4106 <jake.d.rubach@gmail.com>
2022-03-18 13:11:44 -0400

    Functioning edit profile. Style changes. Setup checkout webhook and handlers for use later.

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
@@ -42,6 +42,47 @@ def profile(request):
         'profile': profile,
         'user': user,
         'orders': orders,
+        'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_profile(request):
+    """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    user = get_object_or_404(User, username=request.user)
+
+    form = UserProfileForm(instance=profile)
+    form_two = UserForm(instance=user)
+
+    template = 'profiles/edit-profile.html'
+    
+    context = {
+        'page': 'profile',
+        'form': form,
+        'form_two': form_two,
+        'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_profile(request):
+    '''Delete user profile'''
+    if request.method == "POST":
+    
+        user = get_object_or_404(User, username=request.user)
+        user.delete()
+
+        return redirect(reverse('home'))
+    
+    template = "profiles/delete_profile.html"
+
+    context= {
+        'page':'profile',
         'on_profile_page': True,
     }
 
